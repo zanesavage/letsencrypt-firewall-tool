4ca3daab67a9db0e85849f3567f6391e225e5047
mtereo <matthew.allen.theriault.1996@gmail.com>
2019-01-22 23:20:56 -0500

    Installs checker.sh
    Installs checker.sh on system, schedules to run every Saturday @ 11PM

---
+++ installer.sh
@@ -0,0 +1,7 @@
+cd /etc
+mkdir letsencrypt-firewall-tool
+cd letsencrypt-firewall-tool
+sudo wget https://raw.githubusercontent.com/mtereo/letsencrypt-firewall-tool/new/master/checker.sh -O /etc/letsencrypt-firewall-tool/checker.sh
+sudo chmod +x /etc/letsencrypt-firewall-tool/checker.sh
+crontab -l > mycron
+echo "0 23 * * 6 "/etc/letsencrypt-firewall-tool/checker.sh"
