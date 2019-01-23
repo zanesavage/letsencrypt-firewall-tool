f840a469812108c80ac4898fc35e5655c56a6fc1
mtereo <matthew.allen.theriault.1996@gmail.com>
2019-01-22 23:34:29 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -4,4 +4,6 @@ cd letsencrypt-firewall-tool
 sudo wget https://raw.githubusercontent.com/mtereo/letsencrypt-firewall-tool/new/master/checker.sh -O /etc/letsencrypt-firewall-tool/checker.sh
 sudo chmod +x /etc/letsencrypt-firewall-tool/checker.sh
 crontab -l > mycron
-echo "0 23 * * 6 "/etc/letsencrypt-firewall-tool/checker.sh"
+echo "0 23 * * 6 /etc/letsencrypt-firewall-tool/checker.sh" >> mycron
+crontab mycron
+rm mycron
